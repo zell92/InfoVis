61526a0745718d8465bdd195f2431ca730ff7f45
giuseppe <giuseppe@localhost>
2017-07-31 18:59:54 +0200

    cliques 99%

--- progetto infovis/WebContent/webPages/js/graphCreatorCliques.js
+++ progetto infovis/WebContent/webPages/js/graphCreatorCliques.js
@@ -12,8 +12,8 @@ function createGraphCliques(graph, selectedNodesSimilar) {
 	// create cliques set
 	var array_u2c = LoadFile('frm_1');
 	for (var i = 0; i < array_u2c.length; i++) {
-		if (array_u2c[1].length > 1 && (selectedNodesSimilar.indexOf(array_u2c[i][0]) != -1)) {
-			for (var h = 1; h < array_u2c[1].length; h++) {
+		if (array_u2c[i].length > 1 && (selectedNodesSimilar.indexOf(array_u2c[i][0]) != -1)) {
+			for (var h = 1; h < array_u2c[i].length; h++) {
 				if (array_u2c[i][h].length > 0) {
 					cliquesSet.add(array_u2c[i][h]);
 				}
@@ -25,9 +25,9 @@ function createGraphCliques(graph, selectedNodesSimilar) {
 	var array_c2u = LoadFile('frm_2');
 	for (var i = 0; i < array_c2u.length; i++) {
 		
-		if (array_c2u[1].length > 1) {
+		if (array_c2u[i].length > 1) {
 			alert("finito");
-			for (var h = 1; h < array_c2u[1].length; h++) {
+			for (var h = 1; h < array_c2u[i].length; h++) {
 				if (array_c2u[i][h].length > 0) {
 					usersSet.add(array_c2u[i][h]);
 				}
@@ -35,13 +35,12 @@ function createGraphCliques(graph, selectedNodesSimilar) {
 		}
 	}
 	
-	
 	// create users node
 	var array_follower = LoadFile('frm_3');
 	graph.clear();
 	for (var i = 0; i < array_follower.length; i++) {
-		if (array_follower[1].length > 1 && usersSet.has(array_follower[i][0])) {
-			for (var h = 1; h < array_follower[1].length; h++)
+		if (array_follower[i].length > 1 && usersSet.has(array_follower[i][0])) {
+			for (var h = 1; h < array_follower[i].length; h++)
 				if (array_follower[i][h].length > 0 && usersSet.has(array_follower[i][h]))
 					graph.addLink(array_follower[i][0], array_follower[i][h]);
 		} else {
